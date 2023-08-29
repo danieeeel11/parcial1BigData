7f765ccad507de2aa158e12c311f69a713c31511
danieeeel11 <111476105+danieeeel11@users.noreply.github.com>
2023-08-29 18:27:27 +0200

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,5 +1,6 @@
 from codigoLambda1 import descargahtml
-from codigoLambda2 import descargacsv
+# from codigoLambda2 import descargacsv
+
 
 def test_answer():
-    assert descargahtml() == True
+    assert descargahtml()
