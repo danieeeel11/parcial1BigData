e453e7c6900e1adcbe7906528a512b16b138ae81
danieeeel11 <111476105+danieeeel11@users.noreply.github.com>
2023-08-29 20:03:22 +0200

    Update codigoLambda2.py

--- codigoLambda2.py
+++ codigoLambda2.py
@@ -1,8 +1,8 @@
 import boto3
 from bs4 import BeautifulSoup
-#import csv
+# import csv
 from datetime import datetime
-#import io
+# import io
 
 
 def descargacsv():
@@ -18,7 +18,8 @@ def descargacsv():
     data_noticias_tiempo = html_tiempo.find_all('article')
 
     # Obtener el contenido HTML de Publimetro
-    obj_elespectador = bucket.Object("headlines/raw/elespectador-" + fecha + ".html")
+    obj_elespectador = bucket.Object("headlines/raw/elespectador-" 
+                                     + fecha + ".html")
     body_elespectador = obj_elespectador.get()['Body'].read()
     html_elespectador = BeautifulSoup(body_elespectador, 'html.parser')
     data_noticias_elespectador = html_elespectador.find_all('article')
@@ -26,7 +27,8 @@ def descargacsv():
     # Generar los datos CSV para El Tiempo
     csv_tiempo = "Categoría, Titular, Enlace\n"
     for article in data_noticias_tiempo:
-        link = "https://eltiempo.com" + article.find('a', class_='title page-link')['href']
+        link = "https://eltiempo.com" + 
+            article.find('a', class_='title page-link')['href']
         category = article['data-seccion']
         title = article['data-name'].replace(",", "")
         csv_tiempo += f"{category}, {title}, {link}\n"
@@ -35,11 +37,11 @@ def descargacsv():
     csv_elespectador = "Categoría, Titular, Enlace\n"
     for article in data_noticias_elespectador:
         link = "https://elespectador.com" + article.find('a')['href']
-        category = link.split('/')[3]  # Assuming the category is the 3rd segment of the URL
+        category = link.split('/')[3]
         title = article.find('a').text.replace(",", "")
         csv_elespectador += f"{category}, {title}, {link}\n"
 
-   # Generar la ruta del archivo CSV y subirlo a S3
+    # Generar la ruta del archivo CSV y subirlo a S3
     csv_tiempo_key = (
         f'bucket/headlines/final/periodico=eltiempo/year={fecha[:4]}/'
         f'month={fecha[5:7]}/day={fecha[8:]}/eltiempo.csv'
@@ -50,8 +52,12 @@ def descargacsv():
     )
 
     s3_client = boto3.client('s3')
-    s3_client.put_object(Body=csv_tiempo.encode('utf-8'), Bucket=bucket, Key=csv_tiempo_key)
-    s3_client.put_object(Body=csv_elespectador.encode('utf-8'), Bucket=bucket, Key=csv_elespectador_key)
+    s3_client.put_object(Body=csv_tiempo.encode('utf-8'), 
+                         Bucket=bucket, 
+                         Key=csv_tiempo_key)
+    s3_client.put_object(Body=csv_elespectador.encode('utf-8'), 
+                         Bucket=bucket, 
+                         Key=csv_elespectador_key)
 
     print("Archivos CSV generados y subidos a S3.")
     return True
