acd7fba1b98d16310c43835213883a4df90a0a21
danieeeel11 <111476105+danieeeel11@users.noreply.github.com>
2023-08-29 16:03:15 -0500

    Update codigoLambda1.py

--- codigoLambda1.py
+++ codigoLambda1.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def descargahtml():
     urls = {
         "eltiempo": "https://www.eltiempo.com/",
-        "elespectador": "https://www.elespectador.com/"
+        "publimetro": "https://www.publimetro.co/"
     }
     # Configurar cliente de S3
     s3_client = boto3.client("s3")
@@ -16,7 +16,7 @@ def descargahtml():
         response = urllib.request.urlopen(url)
         content = response.read()
         # Definir la ruta en S3
-        s3_path = f"bucket/news/raw/{newspaper}-{current_date}.html"
+        s3_path = f"news/raw/{newspaper}-{current_date}.html"
         # Subir contenido a S3
         s3_client.put_object(Bucket="bucketparcial1bd",
                              Key=s3_path,
