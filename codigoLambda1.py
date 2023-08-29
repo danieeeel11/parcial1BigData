67e7df1d8183a9fd50971cf7f9a5db7318b515af
sofojedal <111465717+sofojedal@users.noreply.github.com>
2023-08-29 17:29:31 +0200

    Update codigoLambda1.py

--- codigoLambda1.py
+++ codigoLambda1.py
@@ -3,31 +3,24 @@ import urllib.request
 from datetime import datetime
 
 def descargahtml():
-
-    # Definir URLs de los periódicos
-    urls = {
-        "eltiempo": "https://www.eltiempo.com/",
-        "elespectador": "https://www.elespectador.com/"
-    }
-
-    # Configurar cliente de S3
-    s3_client = boto3.client("s3")
-    
-    # Obtener fecha actual
-    current_date = datetime.now().strftime("%Y-%m-%d")
-    
-    for newspaper, url in urls.items():
-    
-        response = urllib.request.urlopen(url)
-        
-	content = response.read()
-	    
-	# Definir la ruta en S3
-	s3_path = f"bucket/news/raw/{newspaper}/{current_date}.html"
-	    
-	# Subir contenido a S3
-	s3_client.put_object(Bucket="bucketparcial1", Key=s3_path, Body=content)
-	    
-	print(f"Contenido de {newspaper} guardado en S3: {s3_path}")
-	    
-    return True
+	# Definir URLs de los periódicos
+	urls = {
+		"eltiempo": "https://www.eltiempo.com/",
+        	"elespectador": "https://www.elespectador.com/"
+    	}
+	# Configurar cliente de S3
+	s3_client = boto3.client("s3")
+	
+	# Obtener fecha actual
+	current_date = datetime.now().strftime("%Y-%m-%d")
+	
+	for newspaper, url in urls.items():
+		response = urllib.request.urlopen(url)
+	      	content = response.read()
+		
+		# Definir la ruta en S3
+		s3_path = f"bucket/news/raw/{newspaper}/{current_date}.html"
+		# Subir contenido a S3
+		s3_client.put_object(Bucket="bucketparcial1", Key=s3_path, Body=content)
+		print(f"Contenido de {newspaper} guardado en S3: {s3_path}")
+	return True
