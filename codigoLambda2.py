00347921b631fac755282103481e03a02b4764c4
danieeeel11 <111476105+danieeeel11@users.noreply.github.com>
2023-08-29 20:10:09 +0200

    Update codigoLambda2.py

--- codigoLambda2.py
+++ codigoLambda2.py
@@ -28,7 +28,7 @@ def descargacsv():
     csv_tiempo = "Categoría, Titular, Enlace\n"
     for article in data_noticias_tiempo:
         link = "https://eltiempo.com" + article.find('a',
-                                                     class_='title page-link')['href']
+                    class_='title page-link')['href']
         category = article['data-seccion']
         title = article['data-name'].replace(",", "")
         csv_tiempo += f"{category}, {title}, {link}\n"
